7ef2b8e78be8c662df2df599c261700ea95cac85
Cas Ibrahim <qasiraibrahim@gmail.com>
2020-08-19 23:13:20 -0700

    added test file, moved products and categories to individual modules

---
+++ __tests__/api.test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// use supergoose or supertest
+
+xdescribe('Server Tests', () => {
+
+  it('should start the server at the port defined in .env', () => {
+
+  });
+
+});
+
+xdescribe('MiddleWare Tests', () => {
+
+  it('should', () => {
+
+  });
+
+});
+
+xdescribe('Route Tests', () => {
+
+  it('should', () => {
+
+  });
+
+});
