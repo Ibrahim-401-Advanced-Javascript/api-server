82b427e1da9cb37810b3db8ad58059b0fbd1a844
Cas Ibrahim <qasiraibrahim@gmail.com>
2020-08-20 23:40:30 -0700

    started refactoring from lab 08, added mongo database and began working on collection functions

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -2,20 +2,63 @@
 
 // use supergoose or supertest
 const { server } = require('../lib/server.js');
-const supergoose = require('@code-fellows/supergoose');
-const mockRequest = supergoose(server);
+const supertest = require('supertest');
+const mockRequest = supertest(server);
 
-xdescribe('Server Tests', () => {
+describe('Server Tests', () => {
 
   it('should respond with 500 on an error', () => {
+
+    return mockRequest
+      .get('/expectedfailure')
+      .then(results => {
+        expect(results.status).toBe(500);
+      })
+      .catch(() => { expect(1).toStrictEqual(2); });
 
   });
 
   it('should respond with 404 on an invalid route', () => {
 
+    return mockRequest
+      .get('/invalidroute')
+      .then(results => {
+        expect(results.status).toBe(404);
+      })
+      .catch(() => { expect(1).toStrictEqual(2); });
+
   });
 
   it('should respond with 404 on an invalid method', () => {
+
+    return mockRequest
+      .submit('/categories')
+      .then(results => {
+        expect(results.status).toBe(404);
+      })
+      .catch(() => { expect(1).toStrictEqual(2); });
+
+  });
+
+  it('should respond properly with a request to /categories', () => {
+
+    return mockRequest
+      .get('/categories')
+      .then(results => {
+        expect(results.status).toBe(200);
+      })
+      .catch(() => { expect(1).toStrictEqual(2); });
+
+  });
+
+  it('should respond properly with a request to /products', () => {
+
+    return mockRequest
+      .get('/products')
+      .then(results => {
+        expect(results.status).toBe(200);
+      })
+      .catch(() => { expect(1).toStrictEqual(2); });
 
   });
 
